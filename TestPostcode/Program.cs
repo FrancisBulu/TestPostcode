134a69bd7ca4272385e5efa916ae30f036e1333b
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2020-11-08 22:12:37 +0100

    Add project files.

---
+++ TestPostcode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPostcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef de postcode in");
+            string line = new String('*', 25);
+            Console.WriteLine(line);
+            int[] postCode = { 1000, 2000, 3000, 4000, 5000, 6000 };
+            string[] stad = { "Brussel", "Antwerpen", "Bergen", "Aalst", "Ninove", "Dilbeek" };
+            int value = int.Parse(Console.ReadLine());
+            int pos = Array.IndexOf(postCode, value);
+            Console.WriteLine($"De stad van die postcode is: {stad[pos]}");
+        }
+    }
+}
